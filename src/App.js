8b4084405aceb9c78015bce2fcb7f1d742ee3d71
shani barak <shanibrk@gmail.com>
2023-01-12 19:30:58 +0200

    add mouse function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Sky } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
 import { Tree } from "./components/Tree";
 import { Player } from "./components/Player";
+import { PersonView } from "./components/PersonView";
 
 function App() {
   useEffect(() => {});
@@ -14,11 +15,13 @@ function App() {
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
         <ambientLight intensity={0.5} />
+        <PersonView />
         <Physics>
           <Player />
           <Tree />
         </Physics>
       </Canvas>
+      <div className="cursor centered absolute">+</div>
     </>
   );
 }
