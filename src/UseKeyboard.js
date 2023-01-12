ddc34aa220b93ce57a9f528ed1b5c2fa89269505
shani barak <shanibrk@gmail.com>
2023-01-12 18:52:36 +0200

    adding actions of front bacl left and right

---
+++ src/UseKeyboard.js
@@ -0,0 +1,65 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+function actionByKey(key) {
+  const keyActionMap = {
+    KeyW: "moveForward",
+    KeyS: "moveBackword",
+    KeyA: "moveLeft",
+    KeyD: "moveRight",
+    Space: "jump",
+    Digit1: "wood",
+    Digit2: "tree",
+    Digit3: "stone",
+    Digit4: "ground",
+    Digit5: "groundGrass",
+  };
+  return keyActionMap[key];
+}
+export const UseKeyboard = () => {
+  const [actions, setActions] = useState({
+    moveForward: false,
+    moveBackword: false,
+    moveLeft: false,
+    moveRight: false,
+    jump: false,
+    texture1: false,
+    texture2: false,
+    texture3: false,
+    texture4: false,
+    texture5: false,
+  });
+
+  const handleKeyDown = useCallback((e) => {
+    const action = actionByKey(e.code);
+    if (action) {
+      setActions((prev) => {
+        return {
+          ...prev,
+          [action]: true,
+        };
+      });
+    }
+  }, []);
+  const handleKeyUp = useCallback((e) => {
+    const action = actionByKey(e.code);
+    if (action) {
+      setActions((prev) => {
+        return {
+          ...prev,
+          [action]: false,
+        };
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [handleKeyDown, handleKeyUp]);
+
+  return actions;
+};
--- src/components/Player.js
+++ src/components/Player.js
@@ -2,14 +2,31 @@ import { useSphere } from "@react-three/cannon";
 import { useFrame, useThree } from "@react-three/fiber";
 import React, { useEffect, useRef } from "react";
 import { Vector3 } from "three";
+import { UseKeyboard } from "../UseKeyboard";
+
+const JUMP = 4;
+const SPEED = 4;
 
 export const Player = () => {
+  const { moveBackword, moveForward, moveLeft, moveRight, jump } =
+    UseKeyboard();
+  //   console.log(
+  //     "actions",
+  //     Object.entries(actions).filter(([k, v]) => v)
+  //   );
+
   const { camera } = useThree();
   const [ref, api] = useSphere(() => ({
     mass: 1,
     type: "Dynamic",
-    position: [0, 0, 10],
+    position: [0, 10, 0],
   }));
+
+  const vel = useRef([0, 0, 0]);
+
+  useEffect(() => {
+    api.velocity.subscribe((v) => (vel.current = v));
+  }, [api.velocity]);
 
   const pos = useRef([0, 0, 0]);
 
@@ -21,6 +38,28 @@ export const Player = () => {
     camera.position.copy(
       new Vector3(pos.current[0], pos.current[1], pos.current[2])
     );
+    const direction = new Vector3();
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackword ? 1 : 0) - (moveForward ? 1 : 0)
+    );
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    );
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(SPEED)
+      .applyEuler(camera.rotation);
+
+    api.velocity.set(direction.x, vel.current[1], direction.z);
+    if (jump && Math.abs(vel.current[1] < 0.02)) {
+      api.velocity.set(vel.current[0], JUMP, vel.current[2]);
+    }
   });
   return <mesh ref={ref}></mesh>;
 };
