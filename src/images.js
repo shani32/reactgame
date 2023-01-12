721c2eb00fda11a3271c3587ff7816f413b1fef0
shani barak <shanibrk@gmail.com>
2023-01-12 09:19:21 +0200

    initial init

---
+++ src/images.js
@@ -0,0 +1,13 @@
+import wood from "./assets/images/wood.png";
+import tree from "./assets/images/tree.png";
+import stone from "./assets/images/stone.png";
+import ground from "./assets/images/ground.png";
+
+const images = {
+  wood,
+  tree,
+  stone,
+  ground,
+};
+
+export default images;
