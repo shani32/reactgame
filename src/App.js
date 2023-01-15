de32c8fd66abd95ccd31e63d409e02cdb8ea888b
shani barak <shanibrk@gmail.com>
2023-01-15 11:48:24 +0200

    change view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import { Sky } from "@react-three/drei";
+import { Sky, Stars } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
-import { Tree } from "./components/Tree";
+import { Stone, Tree } from "./components/Tree";
 import { Player } from "./components/Player";
 import { PersonView } from "./components/PersonView";
 import { Cubes } from "./components/Cubes";
@@ -16,13 +16,29 @@ function App() {
     <>
       <div className="app"></div>
       <Canvas>
-        <Sky sunPosition={[100, 100, 20]} />
+        {/* <Sky sunPosition={[100, 100, 20]} /> */}
+        <Sky
+          distance={450000}
+          sunPosition={[0, -1, 0]}
+          inclination={0}
+          azimuth={0.25}
+        />
+        <Stars
+          radius={100}
+          depth={50}
+          count={5000}
+          factor={4}
+          saturation={0}
+          fade
+          speed={1}
+        />
         <ambientLight intensity={0.5} />
         <PersonView />
         <Physics>
           <Player />
           <Cubes />
           <Tree />
+          <Stone />
         </Physics>
       </Canvas>
       <div className="cursor centered absolute">+</div>
--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { usePlane } from "@react-three/cannon";
-import { treeTexture } from "../textures";
+import { treeTexture, woodTexture, stoneTexture } from "../textures";
 import { NearestFilter, PlaneGeometry, RepeatWrapping } from "three";
 import { UseStore } from "../UseStore";
 
 export const Tree = () => {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
-    position: [0, -0.5, 0],
+    position: [0, 0, 0],
   }));
 
   const [addCube] = UseStore((state) => [state.addCube]);
 
-  treeTexture.magFilter = NearestFilter;
-  treeTexture.wrapS = RepeatWrapping;
-  treeTexture.wrapT = RepeatWrapping;
-  treeTexture.repeat.set(100, 100);
+  woodTexture.magFilter = NearestFilter;
+  woodTexture.wrapS = RepeatWrapping;
+  woodTexture.wrapT = RepeatWrapping;
+  woodTexture.repeat.set(100, 100);
   return (
     <mesh
       onClick={(e) => {
@@ -27,6 +27,32 @@ export const Tree = () => {
     >
       <planeBufferGeometry attach="geometry" args={[100, 100]} />
       <meshStandardMaterial attach="material" map={treeTexture} />
+    </mesh>
+  );
+};
+export const Stone = () => {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+    position: [150, 0, 0],
+  }));
+
+  const [addCube] = UseStore((state) => [state.addCube]);
+
+  stoneTexture.magFilter = NearestFilter;
+  stoneTexture.wrapS = RepeatWrapping;
+  stoneTexture.wrapT = RepeatWrapping;
+  stoneTexture.repeat.set(100, 100);
+  return (
+    <mesh
+      onClick={(e) => {
+        e.stopPropagation();
+        const [x, y, z] = Object.values(e.point).map((val) => Math.ceil(val));
+        addCube(x, y, z);
+      }}
+      ref={ref}
+    >
+      <planeBufferGeometry attach="geometry" args={[200, 200]} />
+      <meshStandardMaterial attach="material" map={stoneTexture} />
     </mesh>
   );
 };
