c72f707d63465b4de0f8f69232f93ca12b2ed945
shani barak <shanibrk@gmail.com>
2023-01-16 11:49:21 +0200

    add instructions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import { useEffect } from "react";
 import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import { Sky, Stars } from "@react-three/drei";
-import { Physics } from "@react-three/cannon";
-import { Tree } from "./components/Tree";
-import { Player } from "./components/Player";
-import { PersonView } from "./components/PersonView";
-import { Cubes } from "./components/Cubes";
-import { TextureType } from "./components/TextureType";
-import { World } from "./components/World";
-import { Stone } from "./components/Stone";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Homepage } from "./components/Homepage";
 import { Game } from "./components/Game";
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,7 +8,21 @@ export const Homepage = () => {
   };
   return (
     <div className="app">
-      <button onClick={handleClick}>Start</button>
+      <div className="instructions">
+        <h3>How to play?</h3>
+        <div className="text">
+          Building world with 5 types of blocks: Grass, Wood, Grass-wood, Glass
+          and Dirt. You can switch blocks with numbers 1-5 on your keyboard. You
+          can navigate the world with the mouse and also with W,A,S,D keys. You
+          can click to add blocks and a;t+Click to remove blocks. You can
+          save/reset your world by clicking the button
+        </div>
+      </div>
+      <div className="game-btn">
+        <button onClick={handleClick}>Start</button>
+      </div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
